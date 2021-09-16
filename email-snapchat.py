48a1c4c92e0a1a065c4bed280e87a063697ef348
vv1ck <75469718+vv1ck@users.noreply.github.com>
2021-09-16 13:01:03 +0300

    Update email-snapchat.py

--- email-snapchat.py
+++ email-snapchat.py
@@ -8,16 +8,12 @@ try:
 except ModuleNotFoundError:
  os.system('pip install time')
 try:
- from colorama import Fore
-except ModuleNotFoundError:
- os.system('pip install colorama')
-try:
  import threading
 except ModuleNotFoundError:
  os.system('pip install threading')
-SP = Fore.YELLOW
-WIT = Fore.WHITE
-RD = Fore.RED
+WIT = "\033[1;37;40m"
+RD = "\033[1;31;40m"
+SP = '\033[1;33;40m'
 r=requests.session()
 print(SP+"""
    ____  By JOKER @vv1ck @TweakPY
@@ -51,27 +47,22 @@ def snapchatCK():
 			QTR.append(pro)
 			JO = str(random.choice(QTR))
 		PROXY = {"https":JO,"http":JO}
-		headers = {
-				'Host': 'accounts.snapchat.com',
-				'Cookie': 'xsrf_token=aDpeseUJS0ysikB9nhdNzA; _ga=GA1.2.113171992.1627308862; _scid=f8244bc8-117d-45aa-b1b0-f24ab31edabc; sc-cookies-accepted=true; Preferences=true; Performance=true; Marketing=true; sc_at=v2|H4sIAAAAAAAAAE3GwRGAMAgEwIqY4cIJxG6MSBUp3m/2teq4YEOlrEuIWpIfSxbD36fB2bFBveEjTDM991H9AatYyihAAAAA; _sctr=1|1627257600000; web_client_id=e64bb4c8-1a1f-4de7-970d-d637c2e9a642',
-				'User-Agent': 'Mozilla/5.0 (@vv1ck) Gecko/20100101 Firefox/90.0',
-				'Accept': 'application/json, text/plain, */*',
-				'Accept-Language': 'en-US,en;q=0.5',
-				'Accept-Encoding': 'gzip, deflate',
-				'Content-Type': 'application/json',
-				'X-Xsrf-Token': 'aDpeseUJS0ysikB9nhdNzA',
-				'Content-Length': '49',
-				'Origin': 'https://accounts.snapchat.com',
-				'Referer': 'https://accounts.snapchat.com/accounts/merlin/login',
-				'Sec-Fetch-Dest': 'empty',
-				'Sec-Fetch-Mode': 'cors',
-				'Sec-Fetch-Site': 'same-origin',
-				'Te': 'trailers',
-				'Connection': 'close'}
-		data = {
-			"email":emal,"app":"BITMOJI_APP"}
+		headers={
+'Host': 'accounts.snapchat.com',
+'Cookie': 'xsrf_token=Fsvfoa9lfV2fzWdckqbU5A; web_client_id=f3f58572-c6f6-47ee-99a1-a4dccfd848d0',
+'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0',
+'Accept': 'application/json, text/plain, */*',
+'Accept-Language': 'en-US,en;q=0.5',
+'Accept-Encoding': 'gzip, deflate',
+'Content-Type': 'application/json',
+'X-Xsrf-Token': 'Fsvfoa9lfV2fzWdckqbU5A',
+'Content-Length': '47',
+'Origin': 'https://accounts.snapchat.com',
+'Referer': 'https://accounts.snapchat.com/accounts/merlin/login',
+'Te': 'trailers',
+'Connection': 'close'} 
 		try:
-			Send = r.post('https://accounts.snapchat.com/accounts/merlin/login',headers=headers,json=data,proxies=PROXY)
+			Send = r.post('https://accounts.snapchat.com/accounts/merlin/login',headers=headers,json={"email":emal,"app":"BITMOJI_APP"},proxies=PROXY)
 			if 'hasSnapchat' in Send.text:
 				print(Fore.GREEN+'[+] linked on snapchat ')
 				print('  Email >> '+emal)
